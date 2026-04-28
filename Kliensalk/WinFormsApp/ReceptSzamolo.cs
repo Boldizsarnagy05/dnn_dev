00aa719e61147f75e77c7a291da7d9bd437a8a72
Bokor Balázs Bence <balazs.bokor@stud.uni-corvinus.hu>
2026-04-29 00:53:50 +0200

    Unit test függvények létrehozása és a tesztek futtatása

--- Kliensalk/WinFormsApp/ReceptSzamolo.cs
+++ Kliensalk/WinFormsApp/ReceptSzamolo.cs
@@ -8,10 +8,36 @@ namespace NaturaCo.RecipeEditor
 {
     public class ReceptSzamolo
     {
-        public double KaloriaKiszamitasa(double osszesKaloria, int adagokSzama)
+        public int teljesIdo(int elkeszites, int sutes)
         {
-            if (adagokSzama <= 0) return 0;
-            else return osszesKaloria / adagokSzama;
+            if (elkeszites < 0 || sutes < 0) return 0;
+            return elkeszites + sutes;
+        }
+
+        public double kaloriaSzamitasa(double osszKaloria, int adag)
+        {
+            if (adag <= 0) return 0;
+            return Math.Round(osszKaloria / adag, 2);
+        }
+
+        public bool ervenyesAdag(int adag)
+        {
+            return adag > 0 && adag <= 100;
+        }
+
+        public bool ervenyesLeiras(string leiras)
+        {
+            if (string.IsNullOrEmpty(leiras)) return true;
+            return leiras.Trim().Length >= 10;
+        }
+        public bool ervenyesIdo(int perc)
+        {
+            return perc >= 0;
+        }
+
+        public bool elegendoHozzavalo(int hozzavalokSzama)
+        {
+            return hozzavalokSzama >= 1;
         }
     }
 }
