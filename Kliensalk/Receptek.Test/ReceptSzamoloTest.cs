51d06a2203489ffe3d929a81acecb02d4109a6e9
Bokor Balázs Bence <balazs.bokor@stud.uni-corvinus.hu>
2026-04-28 21:52:51 +0200

    Alapvető unittest setup-olása, kódolása, futtatása

---
+++ Kliensalk/Receptek.Test/ReceptSzamoloTest.cs
@@ -0,0 +1,29 @@
+﻿using NaturaCo.RecipeEditor;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Receptek.Test
+{
+    public class ReceptSzamoloTest
+    {
+        [Test,
+            TestCase(1000, 4, 250)
+            ]
+        public void KaloriaKiszamitasaTest(double kaloria, int adagok, double vartErtek)
+        {
+            // Arrange
+            var receptSzamolo = new ReceptSzamolo();
+
+            // Act
+            var tenylegesErtek = receptSzamolo.KaloriaKiszamitasa(kaloria, adagok);
+
+            // Assert
+            Assert.AreEqual(vartErtek, tenylegesErtek);
+        }
+    }
+}
